8dbe7482967fa1b96158a5a01ccae5eb216c2fb7
B201079_VươngMinhĐăng <dangvuongminh25@gmail.com>
2022-11-03 12:34:26 +0700

    Cai dat express chay thong bao

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express'); 
+const cors = require('cors');
+const app = express(); 
+const port = 3000; 
+
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res)=>{
+    res.send("Welcome to coffee shop!");
+})
+//Khởi động server
+app.listen(port, ()=>{
+    console.log("Server is running on port " + port);
+})
